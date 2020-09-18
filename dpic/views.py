4c529ea7f8f689a35333a94f928647d98f6aef3b
ramesh878-dj <rameshsolaimalai878@gmail.com>
2020-09-18 15:54:57 +0530

    get mathod changed

--- dpic/views.py
+++ dpic/views.py
@@ -24,24 +24,14 @@ def home(request):
 def addsome(request):
     if request.method == 'GET':
         print('get method')
-        print(request.data)
-        data = request.data 
-        serializer = InstapicSerializer(data=data)
-        if serializer.is_valid():
-            foldername= 'pics/'+data['pic_user_name']
-            root = instaloader.Instaloader(dirname_pattern=foldername,filename_pattern='picvc',save_metadata=False,compress_json =False)
-            user_name = data['pic_user_name']
-            pr=root.download_profile(user_name,profile_pic_only=True)
-            print(pr)
-            serializer.save()
-            mediapath = settings.MEDIA_ROOT+'/'+data['pic_user_name']+'/'
-            myfiles = [ f for f in listdir(mediapath) if isfile(join(mediapath, f))]
-            print(myfiles[0])
-            user_img= '/pics/'+data['pic_user_name']+'/'+myfiles[0]
-            return Response(user_img)
-        else:
-            return Response('null')
-        return Response(serializer.errors, status=200)
+        user_name = Instapic.objects.all()
+        serializer= InstapicSerializer(user_name, many=True)
+        # shutil.rmtree('/ramesh__878')
+        # dirpath='pics/ramesh__878pics/*.jpg'
+        # mediapath = settings. MEDIA_ROOT+'/ramesh__878pics/'
+        # myfiles = [ f for f in listdir(mediapath) if isfile(join(mediapath, f))]
+        # print(myfiles[0])
+        return Response(serializer.data, status=200)
             
 
     if request.method == 'POST':

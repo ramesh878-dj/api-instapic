59cd2e86f4ceef2f142eb75b609de2046aa67c59
ramesh878-dj <rameshsolaimalai878@gmail.com>
2020-09-18 11:38:47 +0530

    dummy front end added

--- instapic/urls.py
+++ instapic/urls.py
@@ -17,12 +17,12 @@ from django.contrib import admin
 from django.urls import path, include
 # from django.conf import settings
 # from django.conf.urls.static import static
-from django.views.generic import TemplateView
+# from django.views.generic import TemplateView
 
 urlpatterns = [
-    path('', TemplateView.as_view(template_name='index.html')),
+    # path('', TemplateView.as_view(template_name='index.html')),
     path('admin/', admin.site.urls),
-    path('api/', include('dpic.urls')),
+    # path('api/', include('dpic.urls')),
 ]
 
 # from django.contrib import admin
